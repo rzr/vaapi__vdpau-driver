581af3910b3d0183bf5152e71969b675bb5f5b5d
Gwenole Beauchesne <gbeauchesne@splitted-desktop.com>
2009-07-23 11:44:50 +0000

    Fully implement delay_usec() for possible future systems, just in case. ;-)

--- src/vdpau_video.c
+++ src/vdpau_video.c
@@ -100,18 +100,92 @@ static const vdpau_image_format_map_t vdpau_image_formats_map[] = {
 #undef DEF
 };
 
+// Get current value of microsecond timer
+static uint64_t get_ticks_usec(void)
+{
+#ifdef HAVE_CLOCK_GETTIME
+    struct timespec t;
+    clock_gettime(CLOCK_REALTIME, &t);
+    return (uint64_t)t.tv_sec * 1000000 + t.tv_nsec / 1000;
+#else
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (uint64_t)t.tv_sec * 1000000 + t.tv_usec;
+#endif
+}
+
+#if defined(__linux__)
+// Linux select() changes its timeout parameter upon return to contain
+// the remaining time. Most other unixen leave it unchanged or undefined.
+#define SELECT_SETS_REMAINING
+#elif defined(__FreeBSD__) || defined(__sun__) || (defined(__MACH__) && defined(__APPLE__))
+#define USE_NANOSLEEP
+#elif defined(HAVE_PTHREADS) && defined(sgi)
+// SGI pthreads has a bug when using pthreads+signals+nanosleep,
+// so instead of using nanosleep, wait on a CV which is never signalled.
+#include <pthread.h>
+#define USE_COND_TIMEDWAIT
+#endif
+
 // Wait for the specified amount of microseconds
 static void delay_usec(unsigned int usec)
 {
-    struct timeval tv;
     int was_error;
 
+#if defined(USE_NANOSLEEP)
+    struct timespec elapsed, tv;
+#elif defined(USE_COND_TIMEDWAIT)
+    // Use a local mutex and cv, so threads remain independent
+    pthread_cond_t delay_cond = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t delay_mutex = PTHREAD_MUTEX_INITIALIZER;
+    struct timespec elapsed;
+    uint64_t future;
+#else
+    struct timeval tv;
+#ifndef SELECT_SETS_REMAINING
+    uint64_t then, now, elapsed;
+#endif
+#endif
+
+    // Set the timeout interval - Linux only needs to do this once
+#if defined(SELECT_SETS_REMAINING)
     tv.tv_sec = 0;
     tv.tv_usec = usec;
+#elif defined(USE_NANOSLEEP)
+    elapsed.tv_sec = 0;
+    elapsed.tv_nsec = usec * 1000;
+#elif defined(USE_COND_TIMEDWAIT)
+    future = get_ticks_usec() + usec;
+    elapsed.tv_sec = future / 1000000;
+    elapsed.tv_nsec = (future % 1000000) * 1000;
+#else
+    then = get_ticks_usec();
+#endif
 
     do {
 	errno = 0;
+#if defined(USE_NANOSLEEP)
+	tv.tv_sec = elapsed.tv_sec;
+	tv.tv_nsec = elapsed.tv_nsec;
+	was_error = nanosleep(&tv, &elapsed);
+#elif defined(USE_COND_TIMEDWAIT)
+	was_error = pthread_mutex_lock(&delay_mutex);
+	was_error = pthread_cond_timedwait(&delay_cond, &delay_mutex, &elapsed);
+	was_error = pthread_mutex_unlock(&delay_mutex);
+#else
+#ifndef SELECT_SETS_REMAINING
+	// Calculate the time interval left (in case of interrupt)
+	now = get_ticks_usec();
+	elapsed = now - then;
+	then = now;
+	if (elapsed >= usec)
+	    break;
+	usec -= elapsed;
+	tv.tv_sec = 0;
+	tv.tv_usec = usec;
+#endif
 	was_error = select(0, NULL, NULL, NULL, &tv);
+#endif
     } while (was_error && (errno == EINTR));
 }
 
