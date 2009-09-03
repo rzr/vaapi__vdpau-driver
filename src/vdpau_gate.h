485d8bae042edaf0d1df5b06e0f363ecc46e8521
Gwenole Beauchesne <gbeauchesne@splitted-desktop.com>
2009-09-03 07:48:17 +0000

    Make it a self-contained header.

--- src/vdpau_gate.h
+++ src/vdpau_gate.h
@@ -22,29 +22,63 @@
 #define VDPAU_GATE_H
 
 #include <vdpau/vdpau.h>
-#include "vdpau_video.h"
+#include <vdpau/vdpau_x11.h>
+
+typedef struct vdpau_vtable       vdpau_vtable_t;
+typedef struct vdpau_vtable      *vdpau_vtable_p;
+typedef struct vdpau_driver_data *vdpau_driver_data_p;
+
+// VDPAU VTable
+struct vdpau_vtable {
+    VdpDeviceDestroy            *vdp_device_destroy;
+    VdpVideoSurfaceCreate       *vdp_video_surface_create;
+    VdpVideoSurfaceDestroy      *vdp_video_surface_destroy;
+    VdpVideoSurfaceGetBitsYCbCr *vdp_video_surface_get_bits_ycbcr;
+    VdpOutputSurfaceCreate      *vdp_output_surface_create;
+    VdpOutputSurfaceDestroy     *vdp_output_surface_destroy;
+    VdpOutputSurfaceGetBitsNative *vdp_output_surface_get_bits_native;
+    VdpVideoMixerCreate         *vdp_video_mixer_create;
+    VdpVideoMixerDestroy        *vdp_video_mixer_destroy;
+    VdpVideoMixerRender         *vdp_video_mixer_render;
+    VdpPresentationQueueCreate  *vdp_presentation_queue_create;
+    VdpPresentationQueueDestroy *vdp_presentation_queue_destroy;
+    VdpPresentationQueueDisplay *vdp_presentation_queue_display;
+    VdpPresentationQueueBlockUntilSurfaceIdle *vdp_presentation_queue_block_until_surface_idle;
+    VdpPresentationQueueQuerySurfaceStatus *vdp_presentation_queue_query_surface_status;
+    VdpPresentationQueueTargetCreateX11 *vdp_presentation_queue_target_create_x11;
+    VdpPresentationQueueTargetDestroy   *vdp_presentation_queue_target_destroy;
+    VdpDecoderCreate            *vdp_decoder_create;
+    VdpDecoderDestroy           *vdp_decoder_destroy;
+    VdpDecoderRender            *vdp_decoder_render;
+    VdpDecoderQueryCapabilities *vdp_decoder_query_capabilities;
+    VdpVideoSurfaceQueryGetPutBitsYCbCrCapabilities *vdp_video_surface_query_ycbcr_caps;
+    VdpOutputSurfaceQueryGetPutBitsNativeCapabilities *vdp_output_surface_query_rgba_caps;
+    VdpGetApiVersion            *vdp_get_api_version;
+    VdpGetInformationString     *vdp_get_information_string;
+    VdpGetErrorString           *vdp_get_error_string;
+};
 
 // VdpGetApiVersion
 VdpStatus
-vdpau_get_api_version(vdpau_driver_data_t *driver_data, uint32_t *api_version)
+vdpau_get_api_version(vdpau_driver_data_p driver_data, uint32_t *api_version)
     attribute_hidden;
 
 // VdpGetInformationString
 VdpStatus
 vdpau_get_information_string(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     const char         **info_string
 ) attribute_hidden;
 
 // VdpGetErrorString
 const char *
-vdpau_get_error_string(vdpau_driver_data_t *driver_data, VdpStatus vdp_status)
+vdpau_get_error_string(vdpau_driver_data_p driver_data, VdpStatus vdp_status)
     attribute_hidden;
 
 // VdpVideoSurfaceCreate
 VdpStatus
 vdpau_video_surface_create(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpDevice            device,
     VdpChromaType        chroma_type,
     uint32_t             width,
@@ -55,14 +89,14 @@ vdpau_video_surface_create(
 // VdpVideoSurfaceDestroy
 VdpStatus
 vdpau_video_surface_destroy(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpVideoSurface      surface
 ) attribute_hidden;
 
 // VdpVideoSurfaceGetBitsYCbCr
 VdpStatus
 vdpau_video_surface_get_bits_ycbcr(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpVideoSurface      surface,
     VdpYCbCrFormat       format,
     uint8_t            **dest,
@@ -72,7 +106,7 @@ vdpau_video_surface_get_bits_ycbcr(
 // VdpOutputSurfaceCreate
 VdpStatus
 vdpau_output_surface_create(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpDevice            device,
     VdpRGBAFormat        rgba_format,
     uint32_t             width,
@@ -83,14 +117,14 @@ vdpau_output_surface_create(
 // VdpOutputSurfaceDestroy
 VdpStatus
 vdpau_output_surface_destroy(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpOutputSurface     surface
 ) attribute_hidden;
 
 // VdpOutputSurfaceGetBitsNative
 VdpStatus
 vdpau_output_surface_get_bits_native(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpOutputSurface     surface,
     const VdpRect       *source_rect,
     uint8_t            **dst,
@@ -100,7 +134,7 @@ vdpau_output_surface_get_bits_native(
 // VdpVideoMixerCreate
 VdpStatus
 vdpau_video_mixer_create(
-    vdpau_driver_data_t          *driver_data,
+    vdpau_driver_data_p           driver_data,
     VdpDevice                     device,
     uint32_t                      feature_count,
     VdpVideoMixerFeature const   *features,
@@ -113,14 +147,14 @@ vdpau_video_mixer_create(
 // VdpVideoMixerDestroy
 VdpStatus
 vdpau_video_mixer_destroy(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpVideoMixer        mixer
 ) attribute_hidden;
 
 // VdpVideoMixerRender
 VdpStatus
 vdpau_video_mixer_render(
-    vdpau_driver_data_t          *driver_data,
+    vdpau_driver_data_p           driver_data,
     VdpVideoMixer                 mixer,
     VdpOutputSurface              background_surface,
     const VdpRect                *background_source_rect,
@@ -141,7 +175,7 @@ vdpau_video_mixer_render(
 // VdpPresentationQueueCreate
 VdpStatus
 vdpau_presentation_queue_create(
-    vdpau_driver_data_t       *driver_data,
+    vdpau_driver_data_p        driver_data,
     VdpDevice                  device,
     VdpPresentationQueueTarget presentation_queue_target,
     VdpPresentationQueue      *presentation_queue
@@ -150,14 +184,14 @@ vdpau_presentation_queue_create(
 // VdpPresentationQueueDestroy
 VdpStatus
 vdpau_presentation_queue_destroy(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpPresentationQueue presentation_queue
 ) attribute_hidden;
 
 // VdpPresentationQueueDisplay
 VdpStatus
 vdpau_presentation_queue_display(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpPresentationQueue presentation_queue,
     VdpOutputSurface     surface,
     uint32_t             clip_width,
@@ -168,7 +202,7 @@ vdpau_presentation_queue_display(
 // VdpPresentationQueueBlockUntilSurfaceIdle
 VdpStatus
 vdpau_presentation_queue_block_until_surface_idle(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpPresentationQueue presentation_queue,
     VdpOutputSurface     surface,
     VdpTime             *first_presentation_time
@@ -177,7 +211,7 @@ vdpau_presentation_queue_block_until_surface_idle(
 // VdpPresentationQueueQuerySurfaceStatus
 VdpStatus
 vdpau_presentation_queue_query_surface_status(
-    vdpau_driver_data_t        *driver_data,
+    vdpau_driver_data_p         driver_data,
     VdpPresentationQueue        presentation_queue,
     VdpOutputSurface            surface,
     VdpPresentationQueueStatus *status,
@@ -187,7 +221,7 @@ vdpau_presentation_queue_query_surface_status(
 // VdpPresentationQueueTargetCreateX11
 VdpStatus
 vdpau_presentation_queue_target_create_x11(
-    vdpau_driver_data_t        *driver_data,
+    vdpau_driver_data_p         driver_data,
     VdpDevice                   device,
     Drawable                    drawable,
     VdpPresentationQueueTarget *target
@@ -196,14 +230,14 @@ vdpau_presentation_queue_target_create_x11(
 // VdpPresentationQueueTargetDestroy
 VdpStatus
 vdpau_presentation_queue_target_destroy(
-    vdpau_driver_data_t       *driver_data,
+    vdpau_driver_data_p        driver_data,
     VdpPresentationQueueTarget presentation_queue_target
 ) attribute_hidden;
 
 // VdpDecoderCreate
 VdpStatus
 vdpau_decoder_create(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpDevice            device,
     VdpDecoderProfile    profile,
     uint32_t             width,
@@ -215,14 +249,14 @@ vdpau_decoder_create(
 // VdpDecoderDestroy
 VdpStatus
 vdpau_decoder_destroy(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpDecoder           decoder
 ) attribute_hidden;
 
 // VdpDecoderRender
 VdpStatus
 vdpau_decoder_render(
-    vdpau_driver_data_t      *driver_data,
+    vdpau_driver_data_p       driver_data,
     VdpDecoder                decoder,
     VdpVideoSurface           target,
     VdpPictureInfo const     *picture_info,
@@ -233,7 +267,7 @@ vdpau_decoder_render(
 // VdpDecoderQueryCapabilities
 VdpStatus
 vdpau_decoder_query_capabilities(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpDevice            device,
     VdpDecoderProfile    profile,
     VdpBool             *is_supported,
@@ -246,7 +280,7 @@ vdpau_decoder_query_capabilities(
 // VdpVideoSurfaceQueryGetPutBitsYCbCrCapabilities
 VdpStatus
 vdpau_video_surface_query_ycbcr_caps(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpDevice            device,
     VdpChromaType        surface_chroma_type,
     VdpYCbCrFormat       bits_ycbcr_format,
@@ -256,7 +290,7 @@ vdpau_video_surface_query_ycbcr_caps(
 // VdpOutputSurfaceQueryGetPutBitsNativeCapabilities
 VdpStatus
 vdpau_output_surface_query_rgba_caps(
-    vdpau_driver_data_t *driver_data,
+    vdpau_driver_data_p  driver_data,
     VdpDevice            device,
     VdpRGBAFormat        surface_rgba_format,
     VdpBool             *is_supported
