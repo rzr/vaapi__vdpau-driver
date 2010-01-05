b1c49b6e2be0ff46596f791e804ee8f7cc1b7f47
Gwenole Beauchesne <gbeauchesne@splitted-desktop.com>
2010-01-05 12:20:14 +0000

    Bump copyright notice years.

--- src/vdpau_decode.c
+++ src/vdpau_decode.c
@@ -1,7 +1,7 @@
 /*
  *  vdpau_decode.c - VDPAU backend for VA API (decoder)
  *
- *  vdpau-video (C) 2009 Splitted-Desktop Systems
+ *  vdpau-video (C) 2009-2010 Splitted-Desktop Systems
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- src/vdpau_video.h
+++ src/vdpau_video.h
@@ -1,7 +1,7 @@
 /*
  *  vdpau_video.h - VDPAU backend for VA API (shareed data)
  *
- *  vdpau-video (C) 2009 Splitted-Desktop Systems
+ *  vdpau-video (C) 2009-2010 Splitted-Desktop Systems
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
