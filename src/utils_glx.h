ee0fd2f0c1bed6660dd48434f9603e35e81e4505
Gwenole Beauchesne <gbeauchesne@splitted-desktop.com>
2010-06-09 16:36:57 +0000

    Add missing files.

---
+++ src/utils_glx.h
@@ -0,0 +1,223 @@
+/*
+ *  utils_glx.h - GLX utilities
+ *
+ *  vdpau-video (C) 2009-2010 Splitted-Desktop Systems
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#ifndef UTILS_GLX_H
+#define UTILS_GLX_H
+
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <GL/glx.h>
+#include <GL/glxext.h>
+
+#if GLX_GLXEXT_VERSION < 18
+typedef void (*PFNGLXBINDTEXIMAGEEXTPROC)(Display *, GLXDrawable, int, const int *);
+typedef void (*PFNGLXRELEASETEXIMAGEEXTPROC)(Display *, GLXDrawable, int);
+#endif
+
+#ifndef GL_FRAMEBUFFER_BINDING
+#define GL_FRAMEBUFFER_BINDING GL_FRAMEBUFFER_BINDING_EXT
+#endif
+#ifndef GL_FRAGMENT_PROGRAM
+#define GL_FRAGMENT_PROGRAM GL_FRAGMENT_PROGRAM_ARB
+#endif
+#ifndef GL_PROGRAM_FORMAT_ASCII
+#define GL_PROGRAM_FORMAT_ASCII GL_PROGRAM_FORMAT_ASCII_ARB
+#endif
+#ifndef GL_PROGRAM_ERROR_POSITION
+#define GL_PROGRAM_ERROR_POSITION GL_PROGRAM_ERROR_POSITION_ARB
+#endif
+#ifndef GL_PROGRAM_ERROR_STRING
+#define GL_PROGRAM_ERROR_STRING GL_PROGRAM_ERROR_STRING_ARB
+#endif
+#ifndef GL_PROGRAM_UNDER_NATIVE_LIMITS
+#define GL_PROGRAM_UNDER_NATIVE_LIMITS GL_PROGRAM_UNDER_NATIVE_LIMITS_ARB
+#endif
+
+const char *
+gl_get_error_string(GLenum error)
+    attribute_hidden;
+
+void
+gl_purge_errors(void)
+    attribute_hidden;
+
+int
+gl_check_error(void)
+    attribute_hidden;
+
+int
+gl_get_current_color(float color[4])
+    attribute_hidden;
+
+int
+gl_get_param(GLenum param, unsigned int *pval)
+    attribute_hidden;
+
+int
+gl_get_texture_param(GLenum target, GLenum param, unsigned int *pval)
+    attribute_hidden;
+
+void
+gl_set_bgcolor(uint32_t color)
+    attribute_hidden;
+
+void
+gl_resize(unsigned int width, unsigned int height)
+    attribute_hidden;
+
+typedef struct _GLContextState GLContextState;
+struct _GLContextState {
+    Display     *display;
+    Window       window;
+    XVisualInfo *visual;
+    GLXContext   context;
+};
+
+GLContextState *
+gl_create_context(Display *dpy, int screen, GLContextState *parent)
+    attribute_hidden;
+
+void
+gl_destroy_context(GLContextState *cs)
+    attribute_hidden;
+
+void
+gl_get_current_context(GLContextState *cs)
+    attribute_hidden;
+
+int
+gl_set_current_context(GLContextState *new_cs, GLContextState *old_cs)
+    attribute_hidden;
+
+void
+gl_swap_buffers(GLContextState *cs)
+    attribute_hidden;
+
+typedef struct _GLTextureState GLTextureState;
+struct _GLTextureState {
+    GLenum          target;
+    GLuint          old_texture;
+    unsigned int    was_enabled : 1;
+    unsigned int    was_bound   : 1;
+};
+
+int
+gl_bind_texture(GLTextureState *ts, GLenum target, GLuint texture)
+    attribute_hidden;
+
+void
+gl_unbind_texture(GLTextureState *ts)
+    attribute_hidden;
+
+GLuint
+gl_create_texture(GLenum target, GLenum format, unsigned int width, unsigned int height)
+    attribute_hidden;
+
+typedef struct _GLVTable GLVTable;
+struct _GLVTable {
+    PFNGLXBINDTEXIMAGEEXTPROC             glx_bind_tex_image;
+    PFNGLXRELEASETEXIMAGEEXTPROC          glx_release_tex_image;
+    PFNGLGENFRAMEBUFFERSEXTPROC           gl_gen_framebuffers;
+    PFNGLDELETEFRAMEBUFFERSEXTPROC        gl_delete_framebuffers;
+    PFNGLBINDFRAMEBUFFEREXTPROC           gl_bind_framebuffer;
+    PFNGLGENRENDERBUFFERSEXTPROC          gl_gen_renderbuffers;
+    PFNGLDELETERENDERBUFFERSEXTPROC       gl_delete_renderbuffers;
+    PFNGLBINDRENDERBUFFEREXTPROC          gl_bind_renderbuffer;
+    PFNGLRENDERBUFFERSTORAGEEXTPROC       gl_renderbuffer_storage;
+    PFNGLFRAMEBUFFERRENDERBUFFEREXTPROC   gl_framebuffer_renderbuffer;
+    PFNGLFRAMEBUFFERTEXTURE2DEXTPROC      gl_framebuffer_texture_2d;
+    PFNGLCHECKFRAMEBUFFERSTATUSEXTPROC    gl_check_framebuffer_status;
+    PFNGLGENPROGRAMSARBPROC               gl_gen_programs;
+    PFNGLDELETEPROGRAMSARBPROC            gl_delete_programs;
+    PFNGLBINDPROGRAMARBPROC               gl_bind_program;
+    PFNGLPROGRAMSTRINGARBPROC             gl_program_string;
+    PFNGLGETPROGRAMIVARBPROC              gl_get_program_iv;
+    PFNGLPROGRAMLOCALPARAMETER4FVARBPROC  gl_program_local_parameter_4fv;
+    PFNGLACTIVETEXTUREPROC                gl_active_texture;
+    PFNGLMULTITEXCOORD2FPROC              gl_multi_tex_coord_2f;
+    unsigned int                          has_texture_from_pixmap : 1;
+    unsigned int                          has_framebuffer_object  : 1;
+    unsigned int                          has_fragment_program    : 1;
+    unsigned int                          has_multitexture        : 1;
+};
+
+GLVTable *
+gl_get_vtable(void)
+    attribute_hidden;
+
+typedef struct _GLPixmapObject GLPixmapObject;
+struct _GLPixmapObject {
+    Display        *dpy;
+    GLenum          target;
+    GLuint          texture;
+    GLTextureState  old_texture;
+    unsigned int    width;
+    unsigned int    height;
+    Pixmap          pixmap;
+    GLXPixmap       glx_pixmap;
+    unsigned int    is_bound    : 1;
+};
+
+GLPixmapObject *
+gl_create_pixmap_object(Display *dpy, unsigned int width, unsigned int height)
+    attribute_hidden;
+
+void
+gl_destroy_pixmap_object(GLPixmapObject *pixo)
+    attribute_hidden;
+
+int
+gl_bind_pixmap_object(GLPixmapObject *pixo)
+    attribute_hidden;
+
+int
+gl_unbind_pixmap_object(GLPixmapObject *pixo)
+    attribute_hidden;
+
+typedef struct _GLFramebufferObject GLFramebufferObject;
+struct _GLFramebufferObject {
+    unsigned int    width;
+    unsigned int    height;
+    GLuint          fbo;
+    GLuint          old_fbo;
+    unsigned int    is_bound    : 1;
+};
+
+GLFramebufferObject *
+gl_create_framebuffer_object(
+    GLenum       target,
+    GLuint       texture,
+    unsigned int width,
+    unsigned int height
+) attribute_hidden;
+
+void
+gl_destroy_framebuffer_object(GLFramebufferObject *fbo)
+    attribute_hidden;
+
+int
+gl_bind_framebuffer_object(GLFramebufferObject *fbo)
+    attribute_hidden;
+
+int
+gl_unbind_framebuffer_object(GLFramebufferObject *fbo)
+    attribute_hidden;
+
+#endif /* UTILS_GLX_H */
