8547bef40fd7c89dd1bf2236b82025767dfb0462
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2012-02-24 14:40:08 +0100

    Fix subpicture blend state (Holger Kaelberer).
    Don't use pre-multiplied alpha blend factors.
    
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>

--- src/vdpau_video_x11.c
+++ src/vdpau_video_x11.c
@@ -612,8 +612,8 @@ render_subpicture(
 
     VdpOutputSurfaceRenderBlendState blend_state;
     blend_state.struct_version                 = VDP_OUTPUT_SURFACE_RENDER_BLEND_STATE_VERSION;
-    blend_state.blend_factor_source_color      = VDP_OUTPUT_SURFACE_RENDER_BLEND_FACTOR_ONE;
-    blend_state.blend_factor_source_alpha      = VDP_OUTPUT_SURFACE_RENDER_BLEND_FACTOR_ONE;
+    blend_state.blend_factor_source_color      = VDP_OUTPUT_SURFACE_RENDER_BLEND_FACTOR_SRC_ALPHA;
+    blend_state.blend_factor_source_alpha      = VDP_OUTPUT_SURFACE_RENDER_BLEND_FACTOR_SRC_ALPHA;
     blend_state.blend_factor_destination_color = VDP_OUTPUT_SURFACE_RENDER_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
     blend_state.blend_factor_destination_alpha = VDP_OUTPUT_SURFACE_RENDER_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
     blend_state.blend_equation_color           = VDP_OUTPUT_SURFACE_RENDER_BLEND_EQUATION_ADD;
